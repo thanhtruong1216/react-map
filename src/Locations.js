b2fc0347445e5fd6ac9f459221b4cafe8791f77d
Thanh Thanh <thanhtruong1216@gmail.com>
2018-05-07 19:20:59 +0700

    Create location list

---
+++ src/Locations.js
@@ -0,0 +1,54 @@
+const locations = [
+  {
+    id: 1,
+    title: 'LotteMart Supermarket',
+    type: 'Supermarket',
+    image: 'http://cdn.onlinewebfonts.com/svg/img_243357.png',
+    location: {
+      lat: 10.741401,
+      lng: 106.702040
+    }
+  },
+  {
+    id: 2,
+    title: 'Ton Duc Thang University',
+    type: 'University',
+    image: 'https://cdn2.iconfinder.com/data/icons/crazy-paparazzi-collection-svg/100/Noun_Project_100Icon_10px_grid_2-47-128.png',
+    location: {
+      lat: 10.733731,
+      lng: 106.699870
+    }
+  },
+  {
+    id: 3,
+    title: 'Vietcombank',
+    type: 'Bank',
+    image: 'https://cdn2.iconfinder.com/data/icons/realty-business/512/euro_bank-512.png',
+    location: {
+      lat: 10.740717,
+      lng: 106.702197
+    }
+
+  },
+  {
+    id: 4,
+    title: 'Starlight Bridge',
+    type: 'Park',
+    image: 'https://d30y9cdsu7xlg0.cloudfront.net/png/12246-200.png',
+    location: {
+      lat: 10.726011,
+      lng: 106.717060
+    }
+  },
+  {
+    id: 5,
+    title: 'Vivo City',
+    type: 'Entertainment Center',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRW8AFE-4wR8t-8pol-_kyuoAd7RX5XbN4nknqTjxhNGLFOFCv8Q',
+    location: {
+      lat: 10.730543,
+      lng: 106.703526
+    }
+  }
+]
+export default locations;
